7ced466624b389a898b379090de14e22cf3b1a69
Patrick <40135835+AlertingAvian@users.noreply.github.com>
2022-09-13 18:26:45 -0500

    Update interface

--- overlay_logo_py/interface/ui.py
+++ overlay_logo_py/interface/ui.py
@@ -1,39 +1,27 @@
 import PySimpleGUI as sg
 
-sg.theme("DarkGrey1")
-
-
-class OverlayUI:
-    @staticmethod
-    def start_event_loop():
-        layout_settings = [
-            [sg.Text("Images:"), sg.InputText(), sg.FilesBrowse()],
-            [sg.Text("Logo:"), sg.InputText(), sg.FileBrowse()],
-            [sg.Text("Save Location"), sg.InputText(), sg.FilesBrowse()],
+class Layouts:
+    layout_settings = [
+            [sg.Text("Images:"), sg.InputText(key="image_paths"), sg.FilesBrowse()],
+            [sg.Text("Logo:"), sg.InputText(key="logo_path"), sg.FileBrowse()],
+            [sg.Text("Save Path:"), sg.InputText(key="save_path"), sg.FolderBrowse()],
             # need selection for logo location
-        ]
-        tab_settings = [
-            [sg.Frame('Settings', layout_settings)]
-        ]
-        # need to go and switch ' with "
-        layout_run = [
-            [sg.Button('Run')]
-            # need more things here
-        ]
-        tab_run = [
-            [sg.Frame("Run", layout_run)]
-        ]
+    ]
+    tab_settings = [
+        [sg.Frame('Settings', layout_settings)]
+    ]
 
-        layout = [
-            [sg.TabGroup([[sg.Tab('Settings', tab_settings), sg.Tab('Run', tab_run)]])],
-            [sg.Exit()]
-        ]
-        window = sg.Window('Overlay Logo', layout)
+    layout_run = [
+        [sg.Button('Run', key="run_overlay")]
+        # need more things here
+        # console output
+        # progress bar
+    ]
+    tab_run = [
+        [sg.Frame("Run", layout_run)]
+    ]
 
-        while True:
-            event, values = window.read()
-
-            print(event, values)
-
-            if event in (None, 'Exit'):
-                break
+    layout = [
+        [sg.TabGroup([[sg.Tab('Settings', tab_settings), sg.Tab('Run', tab_run)]])],
+        [sg.Exit()]
+    ]
---
+++ overlay_logo_py/interface/ui_runner.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+from overlay_logo_py.interface import ui
+
+sg.theme("DarkGrey1")
+
+
+class OverlayUI:
+    @staticmethod
+    def start_event_loop(debug: bool = False):
+        
+        window = sg.Window('Overlay Logo', ui.Layouts.layout)
+
+        while True:
+            event, values = window.read()
+
+            if debug:
+                print(event, values)
+
+            if event in (None, 'Exit'):
+                break
+
